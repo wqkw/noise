a2088e3289bce8d52320bb40a1154b2ceca27c10
Derek Lou <dereklou@Dereks-MBP.lan>
2018-10-06 15:59:30 -0400

    refactor retrieve and Derek's first commit :)

--- retrieve.py
+++ retrieve.py
@@ -1,23 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
 import config as config
-
-# print(config.access_token_secret)
-
-# consumer_key = 'YOUR-CONSUMER-KEY'
-# consumer_secret = 'YOUR-CONSUMER-SECRET'
-# access_token = 'YOUR-ACCESS-TOKEN'
-# access_secret = 'YOUR-ACCESS-SECRET'
-#
-# auth = OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_secret)
-#
-# api = tweepy.API(auth)
-
 import tweepy
 
-auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
+auth = tweepy.OAuthHandler(config.API_KEY, config.API_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
